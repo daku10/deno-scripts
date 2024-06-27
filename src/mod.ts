09e2953e5a8953ded0620b3f5fa48f83b23e1b6b
daku10 <daku10.dev@gmail.com>
2024-06-27 22:17:38 +0900

    feat: cli

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,25 @@
-import { main } from "./make-strict-package-version/main.ts";
+import { parseArgs } from "@std/cli";
 
-main();
+const args = parseArgs(Deno.args);
+const command = args["_"][0];
+if (typeof command !== "string") {
+  console.error("No command provided");
+  Deno.exit(1);
+}
+
+const commandList = Deno.readDir("./src");
+const commands = [];
+for await (const command of commandList) {
+  if (command.isDirectory) {
+    commands.push(command.name);
+  }
+}
+
+if (!commands.includes(command)) {
+  console.error(`Command '${command}' not found`);
+  Deno.exit(1);
+}
+
+const commandPath = `./${command}/main.ts`;
+const commandModule = await import(commandPath);
+commandModule.main();
